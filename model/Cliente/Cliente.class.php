b96f894d621ad71fc9a5ed6ccfd3aada7e582a0b
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-04-15 22:15:42 -0300

    Ajustado função de verificação de mesa

--- model/Cliente/Cliente.class.php
+++ model/Cliente/Cliente.class.php
@@ -9,18 +9,35 @@ abstract class Cliente extends Usuario{
     public function escolherMesa($mesa){
         $conexao = new Conexao;
         $con = $conexao->conexaoPDO();
-        $sql1 = "SELECT count(*) AS total FROM tb_mesa WHERE id_mesa =".$mesa;
+        $sql1 = "SELECT count(*) AS total FROM tb_mesa WHERE id_mesa =".$mesa." and id_cadastro !=".$this->getIdUsuario();
         $mesasUsadas = $con->prepare($sql1);
         $mesasUsadas->execute();
         foreach($mesasUsadas as $c){}
         if($c["total"] == 0){
-            $sql = "INSERT INTO tb_mesa SET id_cadastro = ".$this->getIdUsuario().", id_mesa = $mesa";
-            $mesaEscolhida = $con->prepare($sql);
-            $mesaEscolhida->execute();
+            $sql2 = "SELECT count(*) AS cadastros FROM tb_mesa WHERE id_cadastro =".$this->getIdUsuario();
+            $cadastros = $con->prepare($sql2);
+            $cadastros->execute();
+            foreach($cadastros as $cad)
+            if($cad["cadastros"] != 0)
+                $this->mudarMesa($mesa);
+            else{
+                $sql3 = "INSERT INTO tb_mesa SET id_cadastro = ".$this->getIdUsuario().", id_mesa = $mesa";
+                $mesaEscolhida = $con->prepare($sql3);
+                $mesaEscolhida->execute();
+            }
             return true;
         }else
             return false;
     }
+
+    private function mudarMesa($mesa){
+        $sql = "UPDATE tb_mesa SET id_mesa = $mesa WHERE id_cadastro =".$this->getIdUsuario();
+        $conexao = new Conexao;
+        $con = $conexao->conexaoPDO();
+        $mudar = $con->prepare($sql);
+        $mudar->execute();
+    }
+
     public function escolherPagamento($forma){
 
     }

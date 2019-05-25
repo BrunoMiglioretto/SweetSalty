9273e080e9f5282d07153823235f90efed1f066a
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-05-25 16:50:24 -0300

    Colocando a pasta assets em na pasta view

--- index.php
+++ index.php
@@ -3,28 +3,21 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Sweet Salty</title>
-		<link rel="icon" href="food_premium/img/logo.png" type="image/x-icon">
+		<link rel="icon" href="view/img/logo.png" type="image/x-icon">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--Google Fonts link-->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,600i,700,700i" rel="stylesheet">
-        <link rel="stylesheet" href="assets/css/iconfont.css">
-        <link rel="stylesheet" href="assets/css/slick/slick.css">
-        <link rel="stylesheet" href="assets/css/slick/slick-theme.css">
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/jquery.fancybox.css">
-        <link rel="stylesheet" href="assets/css/bootstrap.css">
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/magnific-popup.css">
-        <!--<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">-->
-        <!--For Plugins external css-->
-        <link rel="stylesheet" href="assets/css/plugins.css" />
-        <!--Theme custom css -->
-        <link rel="stylesheet" href="assets/css/style.css">
-        <!--Theme Responsive css-->
-        <link rel="stylesheet" href="assets/css/responsive.css" />
-        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <link rel="stylesheet" href="view/assets/css/iconfont.css">
+        <link rel="stylesheet" href="view/assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="view/assets/css/jquery.fancybox.css">
+        <link rel="stylesheet" href="view/assets/css/bootstrap.css">
+        <link rel="stylesheet" href="view/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="view/assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="view/assets/css/plugins.css" />
+        <link rel="stylesheet" href="view/assets/css/style.css">
+        <link rel="stylesheet" href="view/assets/css/responsive.css" />
+        <script src="view/assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body data-spy="scroll" data-target=".navbar-collapse">
         <div class="culmn">
@@ -37,73 +30,52 @@
                                     <div class="container-fluid">
                                         <!-- Brand and toggle get grouped for better mobile display -->
                                         <!-- Collect the nav links, forms, and other content for toggling -->
-                                        </div>
                                     </div>
                                 </nav>
-                            </div>	
-                        </div>
+                            </div>
+                        </div>	
                     </div>
                 </div>
             </header> <!--End of header -->
-            <!--home Section -->
-            <section id="home" class="home">
-                <div class="home_skew_border">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-12 ">
-                                <div class="main_home_slider text-center">
-                                    <div class="single_home_slider">
-                                        <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                                            <h1>SWEET SALTY</h1>
-                                            <div class="separator"></div>
-                                            <div class="home_btn">
-                                                <a href="view/logar.php" class="btn btn-default">PORTUGUÊS</a>
-                                                <a href="teste.php" class="btn btn-default">ENGLISH</a>
-                                            </div>
+        </div>
+        <!--home Section -->
+        <section id="home" class="home">
+            <div class="home_skew_border">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 ">
+                            <div class="main_home_slider text-center">
+                                <div class="single_home_slider">
+                                    <div class="main_home wow fadeInUp" data-wow-duration="700ms">
+                                        <h1>SWEET SALTY</h1>
+                                        <div class="separator"></div>
+                                        <div class="home_btn">
+                                            <a href="view/logar.php" class="btn btn-default">PORTUGUÊS</a>
+                                            <a href="teste.php" class="btn btn-default">ENGLISH</a>
                                         </div>
-                                    </div>           
-                                </div>
+                                    </div>
+                                </div>           
                             </div>
                         </div>
-					</div>
-				</div>
-			</section>
-        <!-- START SCROLL TO TOP  -->
+                    </div>
+                </div>
+            </div>
+        </section>
 
-        <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
-        <script src="assets/js/vendor/bootstrap.min.js"></script>
+        <script src="view/assets/js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="view/assets/js/vendor/bootstrap.min.js"></script>
 
-        <script src="assets/js/jquery.magnific-popup.js"></script>
-        <script src="assets/js/jquery.mixitup.min.js"></script>
-        <script src="assets/js/jquery.easing.1.3.js"></script>
-        <script src="assets/js/jquery.masonry.min.js"></script>
+        <script src="view/assets/js/jquery.magnific-popup.js"></script>
+        <script src="view/assets/js/jquery.mixitup.min.js"></script>
+        <script src="view/assets/js/jquery.easing.1.3.js"></script>
+        <script src="view/assets/js/jquery.masonry.min.js"></script>
 
         <!--slick slide js -->
-        <script src="assets/css/slick/slick.js"></script>
-        <script src="assets/css/slick/slick.min.js"></script>
+        <script src="view/assets/css/slick/slick.js"></script>
+        <script src="view/assets/css/slick/slick.min.js"></script>
 
 
-        <script src="assets/js/plugins.js"></script>
-        <script src="assets/js/main.js"></script>
-        <script type="text/javascript">/*
-        	addEventListener("click", function() {
-        	var
-        	el = document.documentElement
-        	, rfs =
-        	el.requestFullScreen
-        	|| el.webkitRequestFullScreen
-        	|| el.mozRequestFullScreen
-        	;
-        	rfs.call(el);
-        	});*/
-        </script>
-        <script type="text/javascript">
-        /*	function start() {
-        		//Alguma coisa nessa linha funcionaria?
-        		//document.getElementById('bt').click;
-        		//alert('Gostaria de abrir a pÃ¡gina em fullscree ao inicializar!');
-        		window.onload=start;
-        	}*/
-        </script>
+        <script src="view/assets/js/plugins.js"></script>
+        <script src="view/assets/js/main.js"></script>
     </body>
 </html>

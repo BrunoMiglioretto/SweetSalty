3ede42ab6c98d606ded4846d378a5f789fc27b66
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-05-26 13:19:53 -0300

    Corrigido o erro de 'Sair' do gerente

--- view/gerente/crudFuncionario/listaFuncionarios.php
+++ view/gerente/crudFuncionario/listaFuncionarios.php
@@ -92,7 +92,7 @@
 			        <div class="modal-body">Ao clicar em "Sair" você será deslogado do sistema</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                        <a class="btn btn-primary" href="../logout.php">Sair</a>
+                        <a class="btn btn-primary" href="../../controller/sairFuncionarioController.php">Sair</a>
                     </div>
 			    </div>
 		    </div>
